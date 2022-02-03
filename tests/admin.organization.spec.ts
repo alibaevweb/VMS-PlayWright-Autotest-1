4eadd8b73e9357d125ad49859d9d49b8e499b884
alibaevweb <alibaevweb@gmail.com>
2022-02-03 15:36:49 +0600

    Организация

--- tests/admin.organization.spec.ts
+++ tests/admin.organization.spec.ts
@@ -1,10 +1,10 @@
 import { test, expect } from '@playwright/test';
-import { org, user } from './testdata';
+import { org, organization, user, object, camera } from './testdata';
 import { HomePage } from '../pages/admin-home-page';
 import { LoginPage } from '../pages/admin-login-page';
 import { OrganizationPage } from "../pages/admin-organization-page";
 
-//Проверить открытья окно создания головной организации
+//Проверить открытья окна создания головной организации
   test('Open create parent organization', async ({ page }) => {
     const homepage = new HomePage(page);
     await homepage.open();
@@ -22,3 +22,16 @@ import { OrganizationPage } from "../pages/admin-organization-page";
     await new OrganizationPage(page).createParentOrganization(org.title, org.bin)
     await expect(locator).toHaveText(['Данные сохранены']);
   });
+
+  
+  //Проверить создания организация
+  test('Create organization', async ({ page }) => {
+    const homepage = new HomePage(page);
+    const locator = page.locator('.notification-message'); 
+    await homepage.open();
+    await new LoginPage(page).login(user.email, user.password)
+    await new OrganizationPage(page).createOrganization(organization.title, organization.bin)
+    await expect(locator).toHaveText(['Данные сохранены']);
+  });
+
+  
